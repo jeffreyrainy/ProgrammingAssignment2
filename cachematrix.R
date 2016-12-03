f6e3a7e1444457f3f9f330f31fa36d764bcc22ee
Jeffrey Rainy <jeff@Jeffreys-MacBook-Pro-2.local>
2016-12-03 13:41:59 -0500

    Commit for Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,62 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix())
+{
+    # variable where we store the cached inverse
+    inv <- NULL
+    
+    # sets the matrix content of our cached inverse matrix
+    # also invalidates the cached inverse
+    set <- function(y)
+    {
+        x <<- y
+        inv <<- NULL
+    }
+    
+    # retrieves the matrix content of our cached inverse matrix
+    get <- function() x
+    
+    # stores the computed inverse
+    setInverse <- function(inverse) inv <<- inverse
+    
+    # retrieves the cached inverse
+    getInverse <- function() inv
+        
+    # offers the four functions above
+    list(
+        set = set, get = get,
+        setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then the cachesolve retrieves the inverse from the cache.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...)
+{
+    # check whether the matrix has an inverse already cached
+    inv <- x$getInverse()
+    
+    # if it has
+    if (!is.null(inv))
+    {
+        # return it, and notify the caller via message 
+        message("Getting cached inverse")
+        return(inv)
+    }
+
+    # otherwise, compute the inverse
+    # passing any extra parameters to the solve function
+    # Caveat, and potential improvement: calling cacheSolve with *different*
+    # extra parameters will not invalidate the cached inverse
+    message("Computing inverse")
+    inv <- solve(x$get(), ...)
+
+    # cache the result
+    x$setInverse(inv)
+    
+    # return the inverse
+    inv
 }
